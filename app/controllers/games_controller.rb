955f943c111de6a85ff5b4d2124cfc3a1a810251
Matt Thompson <matthewlthompson@gmail.com>
2018-01-10 09:49:09 -0500

    Adds name to game params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -69,6 +69,6 @@ class GamesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def game_params
-      params.require(:game).permit(:gametype, :player_minimum, :player_maximum, :playtime, :player_age, :players_best_with, :notes)
+      params.require(:game).permit(:name, :gametype, :player_minimum, :player_maximum, :playtime, :player_age, :players_best_with, :notes)
     end
 end
